52731829f8d29c812a4e7968092e3532894ee0ab
Sven Wolfermann <sven@maddesigns.de>
2014-07-18 12:40:01 +0200

    add gemfile, update package.json and Gruntfile

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'http://rubygems.org'
+
+gem "sass", "~>3.3.10"
+gem "compass", "~>1.0.0.alpha.21"
+gem "singularitygs", "~>1.2.1" #:git => "git://github.com/Team-Sass/Singularity", :branch => "1.x.x"
+gem "breakpoint", "~>2.4.2"
--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,23 @@
 module.exports = function(grunt) {
 
+  //require('time-grunt')(grunt);
+  require('jit-grunt')(grunt);
+
   grunt.initConfig({
+
     devDir: '.tmp',
+    srcDir: 'source',
+    distDir: 'dist/drupal',
+
+    penthouse: {
+      extract : {
+          outfile : '<%= distDir %>/css/critical.css',
+          css : '<%= distDir %>/css/style.css',
+          url : 'http://localhost:3000',
+          width : 1024,
+          height : 768
+      },
+    },
 
     // Sass
     sass: {                              // Task
@@ -14,9 +30,9 @@ module.exports = function(grunt) {
         },
         files: [{
           expand: true,
-          cwd: 'source/css',
+          cwd: '<%= srcDir %>/css',
           src: ['*.scss'],
-          dest: 'dist/css',
+          dest: '<%= distDir %>/css',
           ext: '.css'
         }]
       },
@@ -29,21 +45,82 @@ module.exports = function(grunt) {
         },
         files: [{
           expand: true,
-          cwd: 'source/css',
+          cwd: '<%= srcDir %>/css',
           src: ['*.scss'],
           dest: '<%= devDir %>/css',
           ext: '.css'
         }]
       }
     },
+    // Configuration for autoprefixer
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 version', 'ie 9', 'Android 4', 'Android 2.3']
+      },
+      dev: {
+        options: {
+          map: true
+        },
+        src: '<%= devDir%>/css/style.css'
+      },
+      dist: {
+        src: '<%= distDir %>/css/style.css'
+      }
+    },
+
     // sync
     sync: {
       js: {
         files: [
           {
-            cwd: 'source/js',
+            cwd: '<%= srcDir %>/js',
             dest: '<%= devDir %>/js',
+            src: ['**/*.js']
+          }
+        ]
+      },
+      jsdist: {
+        files: [
+          {
+            cwd: '<%= srcDir %>/js',
+            dest: '<%= distDir %>/js',
             src: ['**/*.js', '!**/_*.js']
+          }
+        ]
+      },
+      img: {
+        files: [
+          {
+            cwd: '<%= srcDir %>/img/',
+            dest: '<%= devDir %>/img/',
+            src: ['**/*']
+          }
+        ]
+      },
+      imgdist: {
+        files: [
+          {
+            cwd: '<%= srcDir %>/img',
+            dest: '<%= distDir %>/img',
+            src: ['**/*']
+          }
+        ]
+      },
+      fonts: {
+        files: [
+          {
+            cwd: '<%= srcDir %>/fonts/',
+            dest: '<%= devDir %>/fonts/',
+            src: ['**/*']
+          }
+        ]
+      },
+      fontsdist: {
+        files: [
+          {
+            cwd: '<%= srcDir %>/fonts',
+            dest: '<%= distDir %>/fonts',
+            src: ['**/*']
           }
         ]
       }
@@ -57,16 +134,32 @@ module.exports = function(grunt) {
             src: ['<%= devDir %>/**/_*.js']
           }
         ]
-      }
+      },
+      dev: {
+        files: [
+          {
+            filter: 'isFile',
+            src: ['<%= devDir %>/**/*']
+          }
+        ]
+      },
+      dist: {
+        files: [
+          {
+            filter: 'isFile',
+            src: ['<%= distDir %>/**/*']
+          }
+        ]
+      },
     },
     // Modernizr configuration
     modernizr: {
       build: {
         devFile: 'remote',
-        outputFile: 'source/js/vendor/_modernizr.js',
+        outputFile: '<%= srcDir %>/js/vendor/_modernizr.js',
         uglify: false,
         files: {
-          src: ['source/**/*.js','source/**/*.scss','!source/js/vendor/*.js']
+          src: ['<%= srcDir %>/**/*.js','<%= srcDir %>/**/*.scss','!<%= srcDir %>/js/vendor/*.js']
         }
       }
     },
@@ -105,13 +198,13 @@ module.exports = function(grunt) {
         options: {
           '-W015': true
         },
-        src: ['source/js/**/*.js', '!source/js/vendor/**/*.js']
+        src: ['<%= srcDir %>/js/**/*.js', '!<%= srcDir %>/js/vendor/**/*.js']
       },
       all: {
         options: {
           '-W015': true, '-W089':true
         },
-        src: ['source/js/**/*.js', '!source/js/vendor/**/*.js']
+        src: ['<%= srcDir %>/js/**/*.js', '!<%= srcDir %>/js/vendor/**/*.js']
       }
     },
     // Configuration for file includes
@@ -125,7 +218,7 @@ module.exports = function(grunt) {
         files: [
           {
             expand: true,
-            cwd: 'source/js',
+            cwd: '<%= srcDir %>/js',
             src: ['**/*.js','!**/_*.js', '!vendor/**/*.js'],
             dest: '<%= devDir %>/js',
             ext: '.js'
@@ -136,9 +229,9 @@ module.exports = function(grunt) {
         files: [
           {
             expand: true,
-            cwd: 'source/js',
-            src: ['**/*.js'],
-            dest: 'dist/js',
+            cwd: '<%= srcDir %>/js',
+            src: ['**/*.js','!**/_*.js', '!vendor/**/*.js'],
+            dest: '<%= distDir %>/js',
             ext: '.js'
           }
         ]
@@ -149,16 +242,28 @@ module.exports = function(grunt) {
         spawn: true
       },
       sync_js: {
-        files: ['source/js/**/*'],
+        files: ['<%= srcDir %>/js/**/*'],
         tasks: ['modernizr:build','sync:js','includes:dev', 'jshint']
       },
+      svg: {
+        files: ['<%= srcDir %>/img/**/*.svg'],
+        tasks: ['newer:svg2png:dev']
+      },
+      img: {
+        files: ['<%= srcDir %>/img/**/*', '!<%= srcDir %>/img/**/*.svg'],
+        tasks: ['sync:img']
+      },
       sass: {
-        files: ['source/css/**/*'],
-        tasks: ['sass:dev']
+        files: ['<%= srcDir %>/css/**/*'],
+        tasks: ['sass:dev', 'autoprefixer:dev',]
       },
       assemble: {
-        files: ['source/templates/**/*.hbs'],
+        files: ['<%= srcDir %>/templates/**/*.hbs'],
         tasks: ['assemble:dev']
+      },
+      svgstore: {
+        files: ['<%= srcDir %>/img/icons/*.svg'],
+        tasks: ['svgstore:dev']
       }
     },
     // browser-sync
@@ -170,7 +275,8 @@ module.exports = function(grunt) {
         options: {
           watchTask: true,
           server: {
-              baseDir: '<%= devDir %>'
+              baseDir: '<%= devDir %>',
+              index: 'overview.html'
           },
           ghostMode: {
             clicks: false,
@@ -184,46 +290,175 @@ module.exports = function(grunt) {
     // Configuration for assemble
     assemble: {
       options: {
-        data: 'source/templates/data/**/*.{json,yml}',
-        helpers: ['source/templates/helpers/**/*.js','handlebars-helper-partial'],
-        layout: ['source/templates/layouts/default.hbs'],
-        partials: 'source/templates/partials/**/*.hbs'
+        data: '<%= srcDir %>/templates/data/**/*.{json,yml}',
+        helpers: ['<%= srcDir %>/templates/helpers/**/*.js'],
+        layout: 'default.hbs',
+        layoutdir: '<%= srcDir %>/templates/layouts',
+        partials: '<%= srcDir %>/templates/partials/**/*.hbs'
       },
       dev: {
         files: [
           {
-            cwd: 'source/templates/pages/',
+            cwd: '<%= srcDir %>/templates/pages/',
             dest: '<%= devDir %>',
             expand: true,
             flatten: true,
             src: ['**/*.hbs']
           }
-        ]
+        ],
+        options: {
+          production: false
+        }
       },
       dist: {
         files: [
           {
-            cwd: 'source/templates/pages/',
-            dest: 'dist/',
+            cwd: '<%= srcDir %>/templates/pages/',
+            dest: '<%= distDir %>/',
             expand: true,
             flatten: true,
             src: ['**/*.hbs']
           }
+        ],
+        options: {
+          production: true
+        }
+      }
+    },
+    // Configuration for uglifying JS
+    uglify: {
+      dist: {
+        options: {
+          compress: {
+            drop_console: true
+          }
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= distDir %>/js',
+          src: ['**/*.js', '!**/_*.js'],
+          dest: '<%= distDir %>/js'
+        }]
+      }
+    },
+    processhtml: {
+      options: {},
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= distDir %>/',
+          src: ['**/*.html'],
+          dest: '<%= distDir %>/'
+        }]
+      }
+    },
+    svg2png: {
+      dev: {
+        files: [{
+            cwd: '<%= srcDir %>/img/',
+            src: ['**/*.svg'],
+            dest: '<%= distDir %>/img/png/',
+            ext: '*.png',
+            expand: false
+          }]
+      },
+      dist: {
+        files: [{
+            cwd: '<%= srcDir %>/img/',
+            src: ['**/*.svg'],
+            dest: '<%= distDir %>/img/png/',
+            ext: '*.png',
+            expand: false
+          }]
+      }
+    },
+    // Configuration for optimizing SVG-files
+    svgmin: {
+      options: {
+         plugins: [
+          { cleanupAttrs: true },
+          { cleanupEnableBackground: true },
+          { cleanupIDs: false },
+          { cleanupNumericValues: true },
+          { collapseGroups: true },
+          { convertColors: true },
+          { convertPathData: true },
+          { convertShapeToPath: true },
+          { convertStyleToAttrs: true },
+          { convertTransform: true },
+          { mergePaths: true },
+          { moveElemsAttrsToGroup: true },
+          { moveGroupAttrsToElems: true },
+          { removeComments: true },
+          { removeDoctype: true },
+          { removeEditorsNSData: true },
+          { removeEmptyAttrs: true },
+          { removeEmptyContainers: true },
+          { removeEmptyText: true },
+          { removeHiddenElems: true },
+          { removeMetadata: true },
+          { removeNonInheritableGroupAttrs: true },
+          { removeRasterImages: true },
+          { removeTitle: false },
+          { removeUnknownsAndDefaults: true },
+          { removeUnusedNS: true },
+          { removeUselessStrokeAndFill: false }, // Enabling this may cause small details to be removed
+          { removeViewBox: false }, // Keep the viewBox because that's where illustrator hides the SVG dimensions
+          { removeXMLProcInst: false }, // Enabling this breaks grunticon because it removes the XML header
+          { sortAttrs: true },
+          { transformsWithOnePath: false } // Enabling this breaks Illustrator SVGs with complex text
+        ]
+      },
+      all: {
+        files: [
+          {
+            cwd: '<%= distDir %>/img/',
+            dest: '<%= distDir %>/img/',
+            expand: true,
+            ext: '.svg',
+            src: ['*.svg']
+          }
         ]
       }
-    }
+    },
+    imageoptim: {
+      pngs: {
+        options: {
+          jpegMini: false,
+          imageAlpha: true,
+          quitAfter: true
+        },
+        src: ['<%= distDir %>/img']
+      }
+    },
+    svgstore: {
+      options: {},
+      dev: {
+        options: {
+          includedemo: true
+        },
+        files: {
+          '<%= devDir %>/img/all.svg': ['<%= srcDir %>/img/icons/*.svg']
+        }
+      },
+      dist: {
+        files: {
+        '<%= distDir %>/img/all.svg': ['<%= srcDir %>/img/icons/*.svg']
+        }
+      }
+    },
+    phantomas: {
+      all : {
+        options : {
+          indexPath: '<%= devDir %>/phantomas/',
+          numberOfRuns: 10,
+          url: 'http://0.0.0.0:3002/'
+        }
+      }
+    },
   });
 
-  grunt.loadNpmTasks('grunt-modernizr');
-  grunt.loadNpmTasks('grunt-includes');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-sync');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-browser-sync');
-  grunt.loadNpmTasks('grunt-assemble');
-
   grunt.registerTask('js', ['modernizr:build','sync:js','includes:dev','jshint']);
-  grunt.registerTask('default', ['sass:dev','assemble:dev','js','browserSync','watch']);
+  grunt.registerTask('default', ['clean:dev','sass:dev','autoprefixer:dev','svgstore:dev','svg2png:dev','sync:img','sync:fonts','js','assemble:dev','browserSync','watch']);
+  grunt.registerTask('dist', ['clean:dist', 'svg2png:dist', 'sync:imgdist','sync:fontsdist','svgstore:dist', 'svgmin:all','sass:dist', 'autoprefixer:dist', 'penthouse', 'modernizr:build', 'sync:jsdist', 'includes:dist', 'uglify:dist','assemble:dist', 'processhtml:dist']);
 };
