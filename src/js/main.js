0ebd6d8fb158df427781730f8f1c073b1d9c26f4
Sven Wolfermann <sven@maddesigns.de>
2015-10-23 11:19:01 +0200

    ms-viewport fix for WP8

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 // import vendor/_modernizr-custom.js
 // import _requireconfig.js
 // import _app.js
+
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement("style");
+    msViewportStyle.appendChild(
+        document.createTextNode(
+            "@-ms-viewport{width:auto!important}"
+        )
+    );
+    document.getElementsByTagName("head")[0].
+        appendChild(msViewportStyle);
+}
